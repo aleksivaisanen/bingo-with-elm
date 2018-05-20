cf8ab9618495a2c63558d326ed88c6ddf55de7d1
Aleksi Väisänen <aleksi.vaisanen@elisanet.fi>
2018-05-20 13:13:38 +0300

    game logic fully working

--- src/Main.elm
+++ src/Main.elm
@@ -24,6 +24,7 @@ main =
 type Model =
     BeginGame
     | Playing Gameboard
+    | GameOver Gameboard
 
 --square type for the gameboard
 type alias Square = 
@@ -48,6 +49,11 @@ gameboard =
     , gameboardNumbers = List.range 1 75
     , playedNumbers = []
     }
+
+--used for checking the diagonal rows
+gameboardDiagonals : List Location
+gameboardDiagonals =
+    [loc 0 0, loc 1 1, loc 2 2, loc 3 3, loc 4 4 , loc 4 0, loc 3 1, loc 2 2, loc 1 3, loc 0 4]
 
 --takes to lists and returns pair generator for shuffling both lists 
 twoRandomLists :List Int -> List Int -> Generator (List Int, List Int)
@@ -103,6 +109,8 @@ playNextNumber model =
                             take 1 gameboard.gameNumbers
                             |> append gameboard.playedNumbers
                     } ! []
+        GameOver gameboard ->
+            model ! []
 
 --shows played numbers
 
@@ -124,10 +132,88 @@ checkWin sqr model =
                 newMatrix =
                     Matrix.map (pressSquare True gameboard sqr) gameboard.gameMatrix
             in
-                Playing {newGameboard |
+                checkWinHelper {newGameboard |
                         gameMatrix = newMatrix
                 } ! []
+        
+        GameOver gameboard-> 
+            model ! []
+
+checkWinHelper : Gameboard -> Model
+checkWinHelper gmbrd =
+    if (checkRows gmbrd) 
+        || (checkRows {gmbrd | gameMatrix = fromList (transpose (toList gmbrd.gameMatrix))})
+        || (checkDiagonals gmbrd) 
+        then    
+        GameOver gmbrd
     
+    else 
+        Playing gmbrd
+
+--function for checking rows in gameboard
+checkRows : Gameboard -> Bool
+checkRows gmbrd =
+    let 
+        listOfLists = toList gmbrd.gameMatrix
+        firstList = concat (take 1 listOfLists)
+    in
+        if (firstList == []) then
+            False
+        else if (length (List.filter (\x -> (List.any (\y -> y == x.number) gmbrd.playedNumbers) && x.pressed) firstList) == 5) then
+            True
+        else
+            checkRows {gmbrd |
+                gameMatrix = fromList (drop 1 listOfLists)}
+
+-- turns cols to rows so we can use the previous function again    
+transpose: List (List a) -> List (List a) 
+transpose listOfLists =
+    case listOfLists of
+        [] ->
+        []
+
+        ([] :: xss) ->
+        transpose xss
+
+        ((x::xs) :: xss) ->
+        let
+            heads =
+            List.filterMap List.head xss
+
+            tails =
+            List.filterMap List.tail xss
+        in
+            (x :: heads) :: transpose (xs :: tails)
+
+--checks diagonal rows    
+checkDiagonals : Gameboard -> Bool
+checkDiagonals gmbrd =  
+    let
+        playedNums = gmbrd.playedNumbers
+        matrix = gmbrd.gameMatrix
+        sqrListOne = List.map (locToSquare matrix) (take 5 gameboardDiagonals)
+        sqrListTwo = List.map (locToSquare matrix) (drop 5 gameboardDiagonals)
+        
+    in
+        if (sqrListOne == [] || sqrListTwo == []) then
+            False
+        else if (length (List.filter (\x -> (List.any (\y -> y == x.number) playedNums) && x.pressed) sqrListOne) == 5) then
+            True
+        else if (length (List.filter (\x -> (List.any (\y -> y == x.number) playedNums) && x.pressed) sqrListTwo) == 5) then
+            True
+        else 
+            False
+
+--helper function for checkDiagonals
+locToSquare : Matrix Square -> Location -> Square
+locToSquare matrix loc =
+    case Matrix.get loc matrix of
+        Just a ->
+            a
+        Nothing ->
+            initSquare -1
+
+
 -- changes Squares pressed value from False to True when used with map 
 pressSquare : Bool -> Gameboard -> Square -> Square -> Square
 pressSquare isPressed gmbrd a b =
@@ -177,6 +263,25 @@ update msg model =
             else
                 model ! []
 
+wrapper : Gameboard -> Html Msg -> Html Msg
+wrapper gmbrd overlay = 
+    div[class "site-wrapper"][
+                h1[][text "Your gameboard"],
+                br[][],
+                div [class "container"] (flatten (Matrix.map createSquare gmbrd.gameMatrix)),
+                button [onClick NextNumber][text "Next number"],
+                showPlayedNumbers gmbrd.playedNumbers,
+                overlay
+
+            ]
+
+playAgainOverlay : Html Msg
+playAgainOverlay = 
+    div[class "congratulations"][
+                h1[][text "You won!"],
+                button [onClick StartGame][text "Want to play again?"] 
+            ]
+
 view : Model -> Html Msg
 view model =
     case model of
@@ -192,11 +297,8 @@ view model =
             ]
 
         Playing gameboard ->
-            div[class "site-wrapper"][
-                h1[][text "Your gameboard"],
-                br[][],
-                div [class "container"] (flatten (Matrix.map createSquare gameboard.gameMatrix)),
-                button [onClick NextNumber][text "Next number"],
-                showPlayedNumbers gameboard.playedNumbers
+            wrapper gameboard (text "")
 
-            ]
+        GameOver gameboard ->
+            wrapper gameboard playAgainOverlay
+            
